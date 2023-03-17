23fc8d9e593475236eb5663de718d6057457e08e
Marc <marc@iwell.nl>
2023-03-17 13:19:36 +0100

    prepared script to extract wind parameters from era5

--- probeersel.py
+++ probeersel.py
@@ -1,11 +1,39 @@
 
 import pandas as pd
 import numpy as np
+import xarray as xr
 
 df = pd.read_csv('data/events.csv', parse_dates=['time_start', 'time_end'], infer_datetime_format=True)
 
-kaasie = np.zeros(df.shape[0])
-for event in df.itertuples():
+prefix_era5_data = 'example_prefix_'
+n = df.shape[0]
+V_mean = np.zeros(n)
+u_mean = np.zeros(n)
+v_mean = np.zeros(n)
+V_max = np.zeros(n)
+u_max = np.zeros(n)
+v_max = np.zeros(n)
+
+
+for i_event, event in enumerate(df.itertuples()):
     print(f'Do something from start time : {event.time_start} until : {event.time_end}')
-    months = pd.date_range(event.time_start, event.time_end + pd.DateOffset(months=1), freq='M')
-    print(months)
+    times = pd.date_range(event.time_start, event.time_end, freq='H')
+    YYYYMM_keys = np.unique([f'{year}{("0" if month < 10 else "")}{month}' for year, month in zip(times.year, times.month)])
+    dss = []
+    for yyyymm in YYYYMM_keys:
+        # ds.append(xr.open_dataset(prefix_era5_data+yyyymm))
+        print('Open '+prefix_era5_data+yyyymm)
+    ds = xr.concat(dss, dim='time')
+
+    # interpolate lons and lats
+    lat = ds.latidude[np.argmin(abs(ds.latitude - event.latitude))]
+    lon = ds.longitude[np.argmin(abs(ds.longitude - event.longitude))]
+    ds = ds.sel(time=times, latitude=lat, longitude=lon)
+    abs_speed = np.hypot(ds.u, ds.v)
+    V_mean[i_event] = np.mean(abs_speed)
+    u_mean[i_event] = np.mean(ds.u)
+    v_mean[i_event] = np.mean(ds.v)
+    V_max[i_event] = np.max(abs_speed)
+    u_max[i_event] = np.max(ds.u)
+    v_max[i_event] = np.max(ds.v)
+
