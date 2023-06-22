aa3d0d6ee3bc8624703ed4bbaa56a85ffd5334a7
Marc <marc@iwell.nl>
2023-06-22 17:14:06 +0200

    analyze velocities

--- analyze_velocities.py
+++ analyze_velocities.py
@@ -2,21 +2,22 @@
 
 import load_data
 import numpy as np
-import analyzer as a
+import toolbox as tb
+import picklemanager as pickm
 import matplotlib.pyplot as plt
 
 ds = load_data.get_ds_drifters('gdp_v2.00.nc_no_sst')
 
 traj_gps = np.where(ds.location_type.values)[0]
-obs_gps = a.obs_from_traj(ds, traj_gps)
+obs_gps = tb.obs_from_traj(ds, traj_gps)
 ds_gps = ds.isel(traj=traj_gps, obs=obs_gps)
 
 traj_argos = np.where(np.invert(ds.location_type.values))[0]
-obs_argos = a.obs_from_traj(ds, traj_argos)
+obs_argos = tb.obs_from_traj(ds, traj_argos)
 ds_argos = ds.isel(traj=traj_argos, obs=obs_argos)
 
-v_gps = a.get_absolute_velocity(ds_gps)
-v_argos = a.get_absolute_velocity(ds_argos)
+v_gps = tb.get_absolute_velocity(ds_gps)
+v_argos = tb.get_absolute_velocity(ds_argos)
 
 mean_err_vn_gps = ds_gps.err_vn.values[np.where(ds_gps.err_vn > 0)[0]].mean()
 std_err_vn_gps = ds_gps.err_vn.values[np.where(ds_gps.err_vn > 0)[0]].std()
@@ -31,3 +32,35 @@ max_v_gps = v_gps.max()
 max_v_argos = v_argos.max()
 print(f'Maximum velocity of GPS = {max_v_gps:.4f} m/s.')
 print(f'Maximum velocity of Argos = {max_v_argos:.4f} m/s.')
+
+#%%
+pickle_name_undrogued = pickm.create_pickle_ds_gdp_name(gps_only=True, undrogued_only=True)
+ds_undrogued = pickm.load_pickle(pickm.create_pickle_path(pickle_name_undrogued))
+
+n_obs = len(ds_undrogued.obs)
+n_traj = len(ds_undrogued.traj)
+
+delta_km = 1
+max_km = 31
+
+distances = np.flip(np.arange(delta_km, max_km, delta_km))
+trajs = np.zeros(len(distances))
+obss = np.zeros(len(distances))
+
+velocities = np.zeros(len(distances))
+
+for i, distance in enumerate(distances):
+    ds = load_data.load_subset(max_aprox_distance_km=distance, min_aprox_distance_km=distance-delta_km, ds=ds_undrogued)
+
+    trajs[i] = len(ds.traj)
+    obss[i] = len(ds.obs)
+    velocities[i] = np.hypot(ds.ve.values, ds.vn.values).mean()
+
+#%%
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.bar(distances, velocities, width=-delta_km*0.8, align='edge', edgecolor='k', color='b')
+ax.set_xlabel('Distance to the shoreline (km)')
+ax.set_ylabel('Mean velocity (m/s)')
+
+plt.savefig(f'figures/bar_plot_mean_velocity_distance_{delta_km}_{max_km}.png')
+plt.show()
--- distance_analysis.py
+++ distance_analysis.py
@@ -16,9 +16,6 @@ obss = np.zeros(len(distances))
 fractions_traj = np.zeros(len(distances))
 fractions_obs = np.zeros(len(distances))
 
-velocities = np.zeros(len(distances))
-
-
 for i, distance in enumerate(distances):
     ds = load_data.load_subset(max_aprox_distance_km=distance, ds=ds_prev)
 
@@ -27,7 +24,6 @@ for i, distance in enumerate(distances):
 
     fractions_traj[i] = len(ds.traj) / n_traj
     fractions_obs[i] = len(ds.obs) / n_obs
-    velocities[i] = np.hypot(ds.ve.values, ds.vn.values).mean()
 
     ds_prev = ds
 
@@ -47,24 +43,3 @@ plt.savefig('figures/distance_percentage_traj_obs_0.png')
 plt.show()
 
 
-#%%
-# plot results
-fig2, ax = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [2, 1]})
-ax[0].plot(distances, fractions_traj*100, c='b', label='Trajectories')
-ax[0].plot(distances, fractions_obs*100, c='r', label='Observations')
-
-ax[0].set_ylabel('Fraction trajectories (%)', color='b')
-ax[0].set_ylabel('Fraction observations (%)', color='r')
-ax[0].set_title('Fractions within distance to the shoreline')
-ax[0].legend(loc='upper left')
-ax[0].grid(True)
-
-
-ax[1].plot(distances, velocities, c='g', label='Mean velocity')
-ax[1].set_ylabel('Mean velocity (m/s)', color='g')
-ax[1].set_xlabel('Distance threshold (km)')
-ax[1].grid(True)
-
-plt.savefig('figures/distance_percentage_traj_obs_0.png')
-
-plt.show()
